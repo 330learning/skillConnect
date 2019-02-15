6c6f74b2bb019189fc8dc006721619d0dddf3c85
Terry Tan <tmy1995hflc@gmail.com>
2019-02-14 18:44:12 -0600

    user login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chip from '@material-ui/core/Chip';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p id="title">skillConnect</p>
+          <TextField
+            id="standard-search"
+            label="Username:"
+            type="search"
+            margin="normal"
+          />
+
+          <TextField
+            id="standard-password-input"
+            label="Password:"
+            type="password"
+            autoComplete="current-password"
+            margin="normal"
+          />
+
+          <Button variant="contained" color="primary" id="sign-in">
+            Sign in
+          </Button>
+          <div id="sign-up-div">
+            <p id="sign-up-p">Don't have an account?  </p>
+            <Button variant="contained" color="secondary" id="sign-up">
+              Sign up
+            </Button>
+          </div>
         </header>
       </div>
     );
