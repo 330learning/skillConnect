2875670d5e6b094fb8909288eba51014edde6677
Terry Tan <tmy1995hflc@gmail.com>
2019-02-26 17:02:48 -0600

    change to class

--- src/Learn/History.js
+++ src/Learn/History.js
@@ -9,37 +9,37 @@ import AsianHistory from "../Images/AsianHistory.jpg";
 import EuropeanHistory from "../Images/EuropeanHistory.jpg";
 
 
-function History() {
+class History extends React.Component {
+    render() {
+        var american = "This course provides a one-year survey of American " +
+            "history from the Colonial Period and the American Revolution to the" +
+            "present day, with an emphasis on the twentieth century.";
 
-    var american = "This course provides a one-year survey of American " +
-    "history from the Colonial Period and the American Revolution to the" +
-    "present day, with an emphasis on the twentieth century.";
+        var african = "This course is designed to introduce students to the social" +
+            "political, economic, and cultural history of Africa from the earliest times to the nineteenth century."
 
-    var african = "This course is designed to introduce students to the social" +
-    "political, economic, and cultural history of Africa from the earliest times to the nineteenth century."
+        var asian = "The course focuses on the history of Asian art from neolithic " +
+            "to modern times, with emphasis on the art forms of the major periods and their " +
+            "relationship to social, political, and religious developments."
 
-    var asian = "The course focuses on the history of Asian art from neolithic "+
-    "to modern times, with emphasis on the art forms of the major periods and their " + 
-    "relationship to social, political, and religious developments."
-
-    var european = "In European History students investigate significant events, individuals, "+ 
-    "developments, and processes in four historical periods from approximately 1450 to the present."
+        var european = "In European History students investigate significant events, individuals, " +
+            "developments, and processes in four historical periods from approximately 1450 to the present."
 
 
 
-    return(
-    <div className= "History">
-        <NavBar lastPage="Courses" to="/Learn" Icon="arrow_back"/>
-        <p id="course-header"> Hisotry Courses </p>
-        <header className = "History-header">
-            <CourseInfoCard name="African History" intro={african} image={AfricanHistory}/>
-            <CourseInfoCard name="American History" intro={american} image={AmericanHistory}/>
-            <CourseInfoCard name="Asian History" intro={asian} image={AsianHistory}/>
-            <CourseInfoCard name="European History" intro={european} image={EuropeanHistory}/>
-        </header>      
-    </div>
-    )
-
+        return (
+            <div className="History">
+                <NavBar lastPage="Courses" to="/Learn" Icon="arrow_back" />
+                <p id="course-header"> Hisotry Courses </p>
+                <header className="History-header">
+                    <CourseInfoCard name="African History" intro={african} image={AfricanHistory} />
+                    <CourseInfoCard name="American History" intro={american} image={AmericanHistory} />
+                    <CourseInfoCard name="Asian History" intro={asian} image={AsianHistory} />
+                    <CourseInfoCard name="European History" intro={european} image={EuropeanHistory} />
+                </header>
+            </div>
+        )
+    }
 
 
 }
--- src/Learn/Learn.js
+++ src/Learn/Learn.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import NavBar from "../Components/NavBar"
 
 import "./Learn.css"
@@ -12,31 +12,30 @@ import Art from "../Images/Art.jpg"
 import Literature from "../Images/Literature.jpg"
 
 
-function Learn(props) {
+class Learn extends React.Component {
+  render() {
+    return (
+      <div className="Learn">
+        <NavBar noBack={true} />
+        <p id="course-header"> Explore Courses</p>
+        <header className="Learn-header">
 
-  return (
-    <div className= "Learn">
-    <NavBar noBack={true}/>
-    <p id="course-header"> Explore Courses</p>
-    <header className = "Learn-header">
+          <CourseCard image={Art} title="Art" letter="A" heartColor="#c5d2e8" />
+          <NavLink to='/Learn/History' style={{ textDecoration: 'none' }}>
+            <CourseCard image={History} title="History" letter="H" heartColor="#c5d2e8" />
+          </NavLink>
+          <CourseCard image={Literature} title="Literature" letter="L" heartColor="#c5d2e8" />
 
-      <CourseCard image={Art} title="Art" letter="A" heartColor="#c5d2e8"/>
-      <NavLink to='/Learn/History' style={{textDecoration:'none'}}>
-      <CourseCard image={History} title="History" letter="H" heartColor="#c5d2e8"/>
-      </NavLink>
-      <CourseCard image={Literature} title="Literature" letter="L" heartColor="#c5d2e8"/>
-    
-    </header>
-    <header className = "Learn-header">
-      <CourseCard image={Math} title="Math" letter="M" heartColor="#c5d2e8"/>
-      <CourseCard image={Programming} title="Programming" letter="P" heartColor="#c5d2e8"/>
-      <CourseCard image={Speech} title="Speech" letter="S" heartColor="#c5d2e8"/>
+        </header>
+        <header className="Learn-header">
+          <CourseCard image={Math} title="Math" letter="M" heartColor="#c5d2e8" />
+          <CourseCard image={Programming} title="Programming" letter="P" heartColor="#c5d2e8" />
+          <CourseCard image={Speech} title="Speech" letter="S" heartColor="#c5d2e8" />
 
-    </header>
-      
-    </div>
-  );
+        </header>
+
+      </div>
+    );
+  }
 }
-
-
 export default Learn;
