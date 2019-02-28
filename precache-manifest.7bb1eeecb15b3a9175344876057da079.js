4b7892df752f071598b035cb335532c0c52cf7f0
Terry Tan <tmy1995hflc@gmail.com>
2019-02-27 22:12:52 -0600

    Updates

---
+++ precache-manifest.7bb1eeecb15b3a9175344876057da079.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "f768d7886dfb04abf82763736eb695e1",
+    "url": "/static/media/Art.f768d788.jpg"
+  },
+  {
+    "revision": "a92d681d44589e112d5f",
+    "url": "/static/css/main.8d80e0ad.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a5edb98a6e0bcf5f570e",
+    "url": "/static/js/2.a5edb98a.chunk.js"
+  },
+  {
+    "revision": "4f1d695adf5432164eb511687906e589",
+    "url": "/static/media/Math.4f1d695a.jpg"
+  },
+  {
+    "revision": "1643191019315a10b0a3b0a84b1851c2",
+    "url": "/static/media/Speech.16431910.jpg"
+  },
+  {
+    "revision": "887a52a8b808d6d135bc5336fc8517f0",
+    "url": "/static/media/Programming.887a52a8.jpg"
+  },
+  {
+    "revision": "bf5a903d07f453412f29330ea0f94a45",
+    "url": "/static/media/History.bf5a903d.jpg"
+  },
+  {
+    "revision": "a92d681d44589e112d5f",
+    "url": "/static/js/main.a92d681d.chunk.js"
+  },
+  {
+    "revision": "eadde6f55c2f9ee1e1ef831e4fe1d737",
+    "url": "/static/media/Literature.eadde6f5.jpg"
+  },
+  {
+    "revision": "311ccce20e13c2f4e89d81f8142d9d53",
+    "url": "/static/media/AmericanHistory.311ccce2.jpg"
+  },
+  {
+    "revision": "a02516aba7f05b76071dbea53a779630",
+    "url": "/static/media/AfricanHistory.a02516ab.jpg"
+  },
+  {
+    "revision": "dde7400b88b60b53f90cb89ead25bb0c",
+    "url": "/static/media/AsianHistory.dde7400b.jpg"
+  },
+  {
+    "revision": "c02af79a02c0c4d69aac74e5da5ce663",
+    "url": "/static/media/EuropeanHistory.c02af79a.jpg"
+  },
+  {
+    "revision": "3b37d8fba1d337911540baad04041f74",
+    "url": "/static/media/1.3b37d8fb.png"
+  },
+  {
+    "revision": "50bf5167800c665d235f6acb4c871570",
+    "url": "/index.html"
+  }
+];
